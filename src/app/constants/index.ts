3a354b871cfc06b0f09beb65ce13b0ece4fb9b3f
bia3334 <quankun3357@gmail.com>
2026-01-01 03:31:53 +0700

    chore: Remove unused barrel export files for app and main constants

--- src/app/constants/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Barrel export for app constants
- */
-
-export * from './settings';
-export * from './shortcuts';
-export * from './tabs';
--- src/main/constants/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Main constants barrel file - export all constants from here
- */
-
-export * from './ai';
-export * from './answer-styles';
-export * from './app';
-export * from './prompts';
-export * from './shortcuts';
